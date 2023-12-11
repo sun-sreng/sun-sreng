6b43dc66ddb8d76535dd59d20de31b0cb5d43d9d
Sun Sreng <sun_sreng@hotmail.com>
2023-12-11 16:45:36 +0700

    fix: delete merged branches

--- my/delete_merged_branches.zsh
+++ my/delete_merged_branches.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 # chmod +x delete_merged_branches.zsh
+# source ~/.zshrc
 
 echo "Fetching branches..."
 # Fetch all branches and prune deleted remote branches
@@ -10,17 +11,25 @@ main_branch=$(git symbolic-ref --short HEAD)
 
 echo "Finding merged branches..."
 # List merged branches except for the main branch
-merged_branches=$(git branch --merged "$main_branch" | grep -v '^\*' | grep -v "$main_branch")
+merged_branches=$(git branch --merged "$main_branch" --format "%(refname:short)" | grep -v '^\*' | grep -v "$main_branch")
 
 # Confirm deletion
 if [[ -n "$merged_branches" ]]; then
     echo "The following branches will be deleted:"
     echo "$merged_branches"
-    read -r -p "Are you sure you want to delete these branches? (y/n) " response
-    if [[ "$response" =~ ^[Yy]$ ]]; then
+    echo -n "Are you sure you want to delete these branches? (y/n): "
+    read response
+    if [[ "$response" == "y" || "$response" == "Y" ]]; then
         # Delete merged branches
-        git branch -d $merged_branches
-        echo "Branches deleted successfully."
+        for branch in $merged_branches; do
+            # Check if branch exists before deletion
+            if git rev-parse --quiet --verify "$branch"; then
+                git branch -d "$branch"
+                echo "Branch '$branch' deleted successfully."
+            else
+                echo "Error: Branch '$branch' not found."
+            fi
+        done
     else
         echo "Deletion aborted."
     fi
